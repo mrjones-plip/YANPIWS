e78051fc3df67a1ac0c434eb9c683e203f14e63b
Ths2-9Y-LqJt6 <mrjones+YANPIWS@plip.com>
2017-03-22 23:32:08 -0700

    0.1 ftw

---
+++ parse_and_save.php
@@ -0,0 +1,50 @@
+#!/usr/bin/php
+<?php
+
+$knownKeys = array(
+    'time',
+    'id',
+    'temperature_F',
+    'humidity',
+);
+$path = "./data";
+
+while($f = fgets(STDIN)){
+    $dataObject = json_decode($f);
+    $saveMeArray = array();
+
+    foreach ($knownKeys as $key) {
+        $saveMeArray[] = cleanseData($dataObject->$key);
+    }
+
+    $today = date('Y-m-d', time());
+    saveArrayToCsv($path, $today, $saveMeArray);
+}
+
+/**
+ * we likely shouldn't trust data sent over unencrypted
+ * un-authenticated channels.  Let's make sure it's on the
+ * up and up. uses regex to replace all but "^0-9\. :-"
+ * @param string $data input to cleanse
+ * @return string
+ */
+function cleanseData($data)
+{
+    $data = trim($data);
+    return preg_replace("/[^0-9\. :-]+/", "", $data);
+}
+
+/**
+ * @param string $path
+ * @param array $array of k/v pairs to save
+ */
+function saveArrayToCsv($path, $file, $array)
+{
+    if (is_dir($path) && is_writable($path . '/' . $file) && is_array($array) && sizeof($array) > 0) {
+        echo 'It\'s ' . $array[2] . ' at ID ' . $array[1] . " - data written to " . $path . '/' . $file ."\n";
+        file_put_contents($path . '/' . $file, implode(',' , $array) . "\n",FILE_APPEND);
+    } else {
+        echo "failed to write to $path - not a dir, not writable or invalid/empty array passed :(";
+    }
+}
+
