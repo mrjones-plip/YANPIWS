6a9390acebacfd31ce6f2ae59208d0a1163578fa
Ths2-9Y-LqJt6 <mrjones@plip.com>
2019-09-19 22:31:34 -0700

    add POST sender

---
+++ read_and_post.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once("config.php");
+
+if(defined('STDIN')){
+    while ($f = fgets(STDIN)) {
+        $dataArray = json_decode($f, true);
+    }
+}
+
+if(isset($YANPIWS['servers']) && is_array($YANPIWS['servers'])){
+    foreach ($YANPIWS['servers'] as $server){
+        if (isset($server['url']) && isset($server['password'])) {
+            $url = $server['url'] . '/parse_and_save.php';
+            $dataArray['password'] = $server['password'];
+
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL,$url);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($dataArray));
+            curl_setopt($ch, CURLOPT_HEADER, true);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+            // for debug or logging
+            $server_output = curl_exec($ch);
+            $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+            curl_close ($ch);
+            error_log("made curl call to $url, got response code $httpcode ");
+        }
+    }
+}
