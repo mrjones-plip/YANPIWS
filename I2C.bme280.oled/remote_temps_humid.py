f638592708e25b0139995df620b50cf2bc4f99d0
mrjones-plip <mrjones-plip@plip.com>
2021-03-24 22:39:15 -0700

    port remote temps humid script

--- I2C.bme280.oled/remote_temps_humid.py
+++ I2C.bme280.oled/remote_temps_humid.py
@@ -1,8 +1,17 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 
 # grab args from CLI
 import argparse
+from luma.core.interface.serial import i2c
+from luma.core.render import canvas
+from luma.oled.device import ssd1306
+from PIL import ImageFont
+import os
+import argparse
+import json
+import time
+import logging.handlers
 
 parser = argparse.ArgumentParser()
 
@@ -28,94 +37,91 @@ yanpiws_temp_2 = args.temp_id2
 
 bus_number = args.bus
 
-humidAndTemp1url = 'http://' + str(yanpiws_ip) + '/ajax.php?content=humidity&id=' + str(yanpiws_temp_1)
-humidAndTemp2url = 'http://' + str(yanpiws_ip) + '/ajax.php?content=humidity&id=' + str(yanpiws_temp_2)
-datetime = 'http://' + str(yanpiws_ip) + '/ajax.php?content=datetime'
+my_logger = logging.getLogger('MyLogger')
+my_logger.setLevel(logging.DEBUG)
+handler = logging.handlers.SysLogHandler(address='/dev/log')
+my_logger.addHandler(handler)
+
 
 def get_string_from_url(url):
     import urllib.request
     raw_html = urllib.request.urlopen(url).read().decode('utf-8').rstrip()
     return raw_html
 
-# fetch the cooked up json -> strings
-import json
-humidAndTemp1 = json.loads(get_string_from_url(humidAndTemp1url))
 
-if humidAndTemp1[0]['temp'] != 'NA':
-    temp1final = str(int(float(humidAndTemp1[0]['temp']))) + '°' + humidAndTemp1[0]['label']
-    if 'humidity' in humidAndTemp1[0]:
-        temp1final = str(int(float(humidAndTemp1[0]['humidity']))) + '% ' + temp1final
-if yanpiws_temp_2 is not None and humidAndTemp1[0]['humidity'] != '' and humidAndTemp1[0]['temp'] != 'NA':
-    humidAndTemp2 = json.loads(get_string_from_url(humidAndTemp2url))
-    temp2final = str(int(float(humidAndTemp2[0]['temp']))) + '°' \
-        + str(humidAndTemp2[0]['label'])
-    if 'humidity' in humidAndTemp2[0] and humidAndTemp2[0]['humidity'] != '':
-        temp2final = str(int(float(humidAndTemp2[0]['humidity']))) + '% ' + temp2final
+def get_humid_and_temp(id):
+    forecastUrl = 'http://' + str(yanpiws_ip) + '/ajax.php?content=humidity&id=' + str(id)
+    return json.loads(get_string_from_url(forecastUrl))
 
-date_time = json.loads(get_string_from_url(datetime))
 
-import os
-import Adafruit_SSD1306
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
+def get_date_time():
+    url = 'http://' + str(yanpiws_ip) + '/ajax.php?content=datetime'
+    date_time = json.loads(get_string_from_url(url))
+    string = date_time['date'] + ' ' + date_time['time']
+    return string
 
-# set full puth for incling libs below
-full_path = os.path.dirname(os.path.abspath(__file__)) + "/"
 
-# Raspberry Pi pin configuration:
-RST = None     # on the PiOLED this pin isnt used
-# Note the following are only used with SPI:
-DC = 23
-SPI_PORT = 0
-SPI_DEVICE = 0
+def show_info(device):
+    my_logger.debug("Weathercaster: remote_all start")
 
-# Rev 2 Pi, Pi 2 & Pi 3 uses bus 1
-# Rev 1 Pi uses bus 0
-# Orange Pi Zero uses bus 0 for pins 1-5 (other pins for bus 1 & 2)
-disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_bus=bus_number)
+    # fetch the cooked up json -> strings
+    humid_and_temp1 = get_humid_and_temp(yanpiws_temp_1)
+    first_line = get_date_time()
+    second_line = ''
+    third_line = ''
 
-# Initialize library.
-disp.begin()
+    if humid_and_temp1[0]['temp'] != 'NA':
+        second_line = str(int(float(humid_and_temp1[0]['temp']))) + '°' + humid_and_temp1[0]['label']
+        if 'humidity' in humid_and_temp1[0]:
+            second_line = str(int(float(humid_and_temp1[0]['humidity']))) + '% ' + second_line
+    if yanpiws_temp_2 is not None and humid_and_temp1[0]['humidity'] != '' and humid_and_temp1[0]['temp'] != 'NA':
+        humid_and_temp2 = get_humid_and_temp(yanpiws_temp_2)
+        third_line = str(int(float(humid_and_temp2[0]['temp']))) + '°' + str(humid_and_temp2[0]['label'])
+        if 'humidity' in humid_and_temp2[0] and humid_and_temp2[0]['humidity'] != '':
+            third_line = str(int(float(humid_and_temp2[0]['humidity']))) + '% ' + third_line
 
-# Clear display.
-disp.clear()
-disp.display()
+    full_path = os.path.dirname(os.path.abspath(__file__)) + "/"
+    font2 = ImageFont.truetype(full_path + "Lato-Heavy.ttf", 12)
+    font1 = ImageFont.truetype(full_path + "Lato-Heavy.ttf", 22)
 
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-width = disp.width
-height = disp.height
-image = Image.new('1', (width, height))
+    my_logger.debug("Weathercaster: remote_all draw")
 
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
+    with canvas(device) as draw:
+        # draw.rectangle(device.bounding_box, outline="white", fill="black")
+        draw.text((0, 0), first_line, font=font2, fill="white")
+        draw.text((0, 17), second_line, font=font1, fill="white")
+        draw.text((0, 41), third_line, font=font1, fill="white")
 
-# Draw a black filled box to clear the image.
-draw.rectangle((-20,-20,width,height), outline=0, fill=0)
 
-# Draw some shapes.
-# First define some constants to allow easy resizing of shapes.
-padding = -2
-top = padding
-bottom = height-padding
+def full_stack():
+    import traceback, sys
+    exc = sys.exc_info()[0]
+    stack = traceback.extract_stack()[:-1]  # last one would be full_stack()
+    if exc is not None:  # i.e. an exception is present
+        del stack[-1]  # remove call of full_stack, the printed exception
+        # will contain the caught exception caller instead
+    trc = 'Traceback (most recent call last):\n'
+    stackstr = trc + ''.join(traceback.format_list(stack))
+    if exc is not None:
+        stackstr += '  ' + traceback.format_exc().lstrip(trc)
+    return stackstr
 
-# Load default font.
-font = ImageFont.truetype(full_path + "Lato-Heavy.ttf", 20)
-font_small = ImageFont.truetype(full_path + "Lato-Heavy.ttf", 12)
-# Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
-# Some other nice fonts to try: http://www.dafont.com/bitmap.php
-# font = ImageFont.truetype('Minecraftia.ttf', 8)
 
-# Draw a black filled box to clear the image.
-draw.rectangle((0,0,width,height), outline=0, fill=0)
+def main(device):
+    while True:
+        show_info(device)
+        time.sleep(5)
 
-# render the data
-draw.text((0, top ), date_time['date'] + ' ' + date_time['time'] , font=font_small, fill=255)
-draw.text((0, top + 18), temp1final , font=font, fill=255)
-if yanpiws_temp_2 is not None:
-    draw.text((0, top + 46), temp2final , font=font, fill=255)
 
-# Display image.
-disp.image(image)
-disp.display()
+if __name__ == "__main__":
+    try:
+        my_logger.debug('Weathercaster: remote_all Starting ')
+        serial = i2c(port=bus_number, address=0x3C)
+        device = ssd1306(serial)
+
+        main(device)
+    except KeyboardInterrupt:
+        my_logger.debug("Weathercaster: remote_all Stopping(Ctrl + C) ")
+        pass
+    finally:
+        my_logger.debug("Weathercaster remote_all exit trace: " + full_stack())
